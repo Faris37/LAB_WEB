5a835d53951ef165aa96189f1ba501fde67c8f84
Faris37 <77088951+Faris37@users.noreply.github.com>
2021-01-07 16:46:41 +0800

    Create registerCheck.php

---
+++ registerCheck.php
@@ -0,0 +1,29 @@
+<?php include ("config.php");
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$gender = $_POST['SEX'];
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+$sql = "INSERT INTO member(NAME,EMAIL,PHONE,GENDER,USERNAME,PASSWORD) values('$name','$email','$phone','$gender','$username','$password')";
+$result = mysqli_query($conn, $sql);
+
+if($result)
+{
+    echo "<script>
+    alert('Member Successfully Registered');
+    window.location.href='index.php';  
+    </script>";
+}
+else
+{
+    echo "<script>
+	alert('Member Failed To Register');
+	window.location.href='register.php';  
+	</script>";
+}
+    
+
+?>

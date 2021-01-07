3adc0aa1cd5e28b5a3d1e9a366abbb743889d935
Faris37 <77088951+Faris37@users.noreply.github.com>
2021-01-07 16:01:03 +0800

    Update loginCheck.php

--- loginCheck.php
+++ loginCheck.php
@@ -1,10 +1,37 @@
-<?php include("config.php");
+<?php include ("config.php");
 
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+
+$query = "select id from sql12385564.member where username = '".$username."' and password = '".$password."'";
+$result = mysqli_query($conn, $query);
+
+
+
+if($result != "")
+   { 
+    if ($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {  
+        $id = $row['id'];
+        }
+    }
+
+    echo "<script>
+	alert('Logged In');
+	window.location.href='mainmenu.php?id=$id';  
+    </script>";
+
+   }
+   
+   else
+   {
+
+    echo "<script>
+	alert('No Username or Password matched');
+	window.location.href='https://golferb031920011.herokuapp.com';  
+	</script>";
+
+   }
+	  
 ?>
-
-<html>
-<head></head>
-<body>
-<h2> ELOK JE </h2>
-</body>
-</html>

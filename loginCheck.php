a4085dd7d2a065d63d6b5b17d776d51457b494f7
Faris37 <77088951+Faris37@users.noreply.github.com>
2021-01-07 13:36:21 +0800

    Update loginCheck.php

--- loginCheck.php
+++ loginCheck.php
@@ -1,4 +1,4 @@
-<?php include('config.php');
+<?php include("config.php");
 
 // Escape user inputs for security
 $username = mysqli_real_escape_string($conn, $_REQUEST['username']);
@@ -7,26 +7,9 @@ $password = mysqli_real_escape_string($conn, $_REQUEST['password']);
 // Attempt insert query execution
 $sql = "SELECT * FROM sql12385564.member where username = '".$username."' and password = '".$password."'";
 $result = mysqli_query($conn, $sql);
-if($result == false){
-    
-    echo $result;
-    echo $username;
-    echo $password;
-    echo $sql;
-	/*echo "<script>
-	alert('Login Successfully');
-	window.location.href='http://localhost/CIMS/login.php';  
-	</script>";*/
-	//header("location: welcome.php");
-} else{
 
-    echo $result;
-   /*echo "<script>
-	alert('No member with the username or password match');
-	window.location.href='https://golferb031920011.herokuapp.com/index.php';  
-	</script>";*/
-}
- 
+echo $result;
+
 // Close connection
 mysqli_close($conn);
 ?>

0f0c6c26013cf58e129d373ca2892d220d687fb2
Faris37 <77088951+Faris37@users.noreply.github.com>
2021-01-07 13:40:58 +0800

    Update loginCheck.php

--- loginCheck.php
+++ loginCheck.php
@@ -1,13 +1,10 @@
 <?php include("config.php");
 
-$username = mysqli_real_escape_string($conn, $_REQUEST['username']);
-$password = mysqli_real_escape_string($conn, $_REQUEST['password']);
-
-
-$query = "SELECT * FROM sql12385564.member where username = '".$username."' and password = '".$password."'";
-$result = mysqli_query($conn, $query);
-
-echo $result;
-
-
 ?>
+
+<html>
+<head></head>
+<body>
+<h2> ELOK JE </h2>
+</body>
+</html>

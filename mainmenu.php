11061e8df52234ea2279c59819fa85b025f0c110
Faris37 <77088951+Faris37@users.noreply.github.com>
2021-01-07 16:01:09 +0800

    Create mainmenu.php

---
+++ mainmenu.php
@@ -0,0 +1,29 @@
+<?php include ("config.php");
+    $id = $_GET['id'];
+?>
+
+<html><head>
+<title>Golfer</title>
+<link rel="stylesheet" href="styles.css">
+</head>
+<body>
+
+<header>
+<hr style="width: 100%; height: 2px;">
+</header>
+
+<nav>
+<a href="mainmenu.php?id=<?php echo $id?>">HOME</a>
+<a href="profile.php?id=<?php echo $id?>">VIEW PROFILE</a>
+<a href="golfer.php?id=<?php echo $id?>">GOLFER</a>
+<a href="index.php">LOGOUT</a>
+</nav>
+
+
+
+<hr style="width: 100%; height: 2px;">
+<footer>
+  <p>Copyright &#169; <a href="mailto:farisizwanfauzi@gmail.com">farisizwanfauzi@gmail.com</a></p>
+</footer>
+
+</body></html>

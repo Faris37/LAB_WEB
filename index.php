9c60c555e26904c0dd23ae8e9bc300617481462a
Faris37 <77088951+Faris37@users.noreply.github.com>
2021-01-07 13:08:13 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,39 @@
 <?php include ("config.php");
 
 ?>
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        body{ font: 14px sans-serif; background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: cover; }
+        .wrapper{ width: 350px; padding: 20px; margin: auto; background-color: white; margin-top: 50px; }
+    </style>
 </head>
 <body>
-
-<h2>Golfer</h2>
-
+    <div class="wrapper">
+        <h2>Login Golfer</h2>
+        <p>Please fill in your credentials to login.</p>
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+            <div class="form-group">
+                <label>Username</label>
+                <input type="text" name="username" class="form-control" value="<?php echo ''; ?>">
+                <span class="help-block"></span>
+            </div>    
+            <div class="form-group">
+                <label>Password</label>
+                <input type="password" name="password" class="form-control">
+                <span class="help-block"></span>
+            </div>
+            <div class="form-group">
+                <input type="submit" class="btn btn-primary" value="Login">
+            </div>
+            
+        </form>
+    </div>    
 </body>
 </html>

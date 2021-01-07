2caf9139d6c8b86c2f1a2f46a3b7c280735964e7
Faris37 <77088951+Faris37@users.noreply.github.com>
2021-01-07 13:39:09 +0800

    Update loginCheck.php

--- loginCheck.php
+++ loginCheck.php
@@ -1,15 +1,13 @@
 <?php include("config.php");
 
-// Escape user inputs for security
 $username = mysqli_real_escape_string($conn, $_REQUEST['username']);
 $password = mysqli_real_escape_string($conn, $_REQUEST['password']);
 
-// Attempt insert query execution
-$sql = "SELECT * FROM sql12385564.member where username = '".$username."' and password = '".$password."'";
-$result = mysqli_query($conn, $sql);
+
+$query = "SELECT * FROM sql12385564.member where username = '".$username."' and password = '".$password."'";
+$result = mysqli_query($conn, $query);
 
 echo $result;
 
-// Close connection
-mysqli_close($conn);
+
 ?>
